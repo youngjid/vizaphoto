a68e41aa62e422c91d65b2cf534b246276fd1645
Jidraph Kimachia <jidkimachia@gmail.com>
2025-04-24 09:16:14 +0300

    Refactor: Remove unused dependencies and v0 references

--- jest.setup.ts
+++ jest.setup.ts
@@ -10,25 +10,6 @@ declare global {
   }
 }
 
-// Mock fabric.js
-jest.mock('fabric', () => ({
-  Canvas: jest.fn().mockImplementation(() => ({
-    add: jest.fn(),
-    remove: jest.fn(),
-    renderAll: jest.fn(),
-    setZoom: jest.fn(),
-    getZoom: jest.fn(),
-    dispose: jest.fn(),
-    on: jest.fn(),
-    getActiveObject: jest.fn(),
-    loadFromJSON: jest.fn(),
-    toJSON: jest.fn(),
-  })),
-  Image: {
-    fromURL: jest.fn(),
-  },
-}));
-
 // Mock next/image
 jest.mock('next/image', () => ({
   __esModule: true,
